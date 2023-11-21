40bafc43a152572263ee18758bea94db60a0f6d2
fmetzler <fmetzler22@gmail.com>
2023-11-21 11:25:51 -0300

    hbs de cart product y licenseadmin vacio falta completar y actualizacionde usariocontrollers

--- src/Controllers/usuariosControllers.js
+++ src/Controllers/usuariosControllers.js
@@ -44,15 +44,24 @@ export const getUserById = async(req, res) =>{
  
 
 export const updateUser = async(req, res) =>{
+   
     const user = req.body
     let result = await usuarioService.updateUser(user)
     res.send({status:"success", result: result})
 }
 
 export const createUser = async(req, res, next) =>{
-    const user = req.body
-    let result = await usuarioService.createUser(user)
-    res.send({status:"success", result: result}) 
+    passport.authenticate('registro', (err,user, info)=>{
+        if(err){
+            console.log(err)
+            return res.status(500).json({error: 'No se pudo registrar, vuelva a intentarlo '})
+        }
+        if(!user){
+            return res.reditect('/register')
+        }
+        return res.redirect('/login')
+    }) (req, res,next)
+    
     
 }
 
@@ -64,6 +73,40 @@ export const deleteUser = async(req,res)=>{
 export const userForm = (req, res)=>{
     res.render('register')}
 
-export const userSession = (req, res) =>{
+export const userSession = (req, res, next) =>{
+    passport.authenticate('login', (err, user, info)=>{
+        req.session.user = {
+            first_name: user.first_name,
+            last_name: user.last_name,
+            age: user.age,
+            email: user.email,
+            cart: user.cart
+        }
+        //si es error que me mande un msj de error y que me direccione al registro
+        if(err){
+            console.error(err)
+            return res.status(500).json({error:'Error al iniciar session, verifique datos o por favor registrarse'})
+            return res.redirect('/register')
+        }
+        //si user.email coincide con admin que me envia al hbs de admin para poder modificar o editar lo que el quisiera y de caso contrario que te direccione al hbs de products para poder hacer una comprar como usuario común.
+        if(user.email ===admin){
+            req.session.admin =true
+            return res.redirect('/licenseadmin')
+        }else{
+            req.session.admin = false
+            res.redirect('/products')
+        }
+        
+
+    })
     res.render('login')
 }
+
+export const login = passport.authenticate('login', {
+    successRedirect: '/successlogin',
+    failureRedirect: '/faillogin',
+})
+export  const logout = (req, res) => {
+    req.logout();
+    res.redirect('/login');
+  };
